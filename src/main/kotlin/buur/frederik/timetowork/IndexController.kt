da664385d7d9e8c5cc1f9d5d91d455007ed55ee8
Frederik <frederik_buur@hotmail.com>
2020-05-20 11:07:02 +0200

    added new paths

--- src/main/kotlin/buur/frederik/timetowork/IndexController.kt
+++ src/main/kotlin/buur/frederik/timetowork/IndexController.kt
@@ -1,14 +1,25 @@
-package buur.frederik.timetowork.api
+package buur.frederik.timetowork
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/")
 @RestController
 class IndexController {
 
-    @GetMapping
-    fun getIndex() = "Server is up and running!"
+    @GetMapping("/")
+    fun getIndex(): String {
+      return "Server is up and running!"
+    }
+    
+    @GetMapping("/user")
+    fun user(): String {
+        return "user page"
+    }
+    
+    @GetMapping("/admin")
+    fun admin(): String {
+        return "admin page"
+    }
 
 }
--- src/main/kotlin/buur/frederik/timetowork/SecurityConfig.kt
+++ src/main/kotlin/buur/frederik/timetowork/SecurityConfig.kt
@@ -1,4 +1,42 @@
 package buur.frederik.timetowork
 
-class SecurityConfig {
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+	
+	override fun configure(auth: AuthenticationManagerBuilder?) {
+		// set config for auth
+		auth?.inMemoryAuthentication()
+				?.withUser("user")
+				?.password("user")
+				?.roles("USER")
+				?.and()
+				?.withUser("admin")
+				?.password("admin")
+				?.roles("ADMIN")
+	}
+	
+	override fun configure(http: HttpSecurity?) {
+		http?.let {
+			it.authorizeRequests() // most restrictive to least restrictive
+					.antMatchers("/admin").hasRole("ADMIN")
+					.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+					.antMatchers("/").permitAll()
+//					.antMatchers("/**").hasRole("ADMIN")
+					.and().formLogin()
+		}
+	}
+	
+	@Bean
+	fun getPasswordEncoder(): PasswordEncoder {
+		return NoOpPasswordEncoder.getInstance()
+	}
+	
 }
