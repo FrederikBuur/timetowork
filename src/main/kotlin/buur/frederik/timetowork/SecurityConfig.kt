a698d1ef6f93ea0eeb93f8923c255dc1885b5637
Frederik <frederik_buur@hotmail.com>
2020-05-30 23:12:38 +0200

    sql stuff

--- src/main/kotlin/buur/frederik/timetowork/SecurityConfig.kt
+++ src/main/kotlin/buur/frederik/timetowork/SecurityConfig.kt
@@ -1,37 +1,44 @@
 package buur.frederik.timetowork
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import javax.sql.DataSource
 
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(
+		@Autowired val dataSource: DataSource
+) : WebSecurityConfigurerAdapter() {
 	
 	override fun configure(auth: AuthenticationManagerBuilder?) {
 		// set config for auth
-		auth?.inMemoryAuthentication()
-				?.withUser("user")
-				?.password("user")
-				?.roles("USER")
-				?.and()
-				?.withUser("admin")
-				?.password("admin")
-				?.roles("ADMIN")
+//		auth?.inMemoryAuthentication()
+//				?.withUser("user")
+//				?.password("user")
+//				?.roles("USER")
+//				?.and()
+//				?.withUser("admin")
+//				?.password("admin")
+//				?.roles("ADMIN")
+		
+		auth?.jdbcAuthentication()
+				?.dataSource(dataSource)
+				?.usersByUsernameQuery("select username,password,enables from users where username = ?") // here you can override how users from db should be queried
+				?.usersByUsernameQuery("select username,authorities from authorities where username = ?") // here you can override how authorities from db should be queried
 	}
 	
 	override fun configure(http: HttpSecurity?) {
-		http?.let {
-			it.authorizeRequests() // most restrictive to least restrictive
-					.antMatchers("/admin").hasRole("ADMIN")
-					.antMatchers("/user").hasAnyRole("USER", "ADMIN")
-					.antMatchers("/").permitAll()
-//					.antMatchers("/**").hasRole("ADMIN")
-					.and().formLogin()
-		}
+		http?.authorizeRequests()
+				?.antMatchers("/admin")?.hasRole("ADMIN")
+				?.antMatchers("/user")?.hasAnyRole("USER", "ADMIN")
+				?.antMatchers("/")?.permitAll()
+				?.and()?.formLogin()
 	}
 	
 	@Bean
